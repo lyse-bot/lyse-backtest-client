730c69c63bfb79d26c67dee3801f31b4082251c4
Clément <clement.dreiski@gmail.com>
2023-02-28 16:47:13 +0100

    Authorizations

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import time
+import base64
 import datetime
+import time
+from getpass import getpass
 
 import requests
 
 HOST = "http://127.0.0.1:8000"
-ENDPOINT = "/backtest/run"
 STRATEGY_NAME = "StevenCohen"
 STRATEGY_FILE = "strategy.py"
 START_TIME = datetime.date(2023, 1, 28)
@@ -18,14 +19,28 @@ def load_strategy() -> str:
     return content
 
 
+def get_authorization():
+    username = input("Username: ")
+    password = getpass(prompt="Password: ", stream=None)
+    return (base64.b64encode((username + ":" + password).encode())).decode()
+
+
 def run():
-    result = requests.post(HOST + ENDPOINT, json={
-        "start_time": (time.mktime(START_TIME.timetuple())),
-        "end_time": (time.mktime(END_TIME.timetuple())),
-        "name": STRATEGY_NAME,
-        "content": content
-    })
-    print(result.json()['result'])
+    result = requests.post(
+        HOST + "/backtest/run",
+        headers={"Authorization": get_authorization()},
+        json={
+            "start_time": (time.mktime(START_TIME.timetuple())),
+            "end_time": (time.mktime(END_TIME.timetuple())),
+            "name": STRATEGY_NAME,
+            "content": content,
+        },
+    )
+    if result.status_code == 200:
+        result = result.json()["result"]
+        print(result)
+    else:
+        print(result.text)
 
 
 if __name__ == "__main__":
